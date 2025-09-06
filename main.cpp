90bf487f736d3285901a8d34211627bf01fb736c
Vladimir Cheskis <vladimir.talmid@gmail.com>
2025-09-06 17:39:36 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
 	std::cout << "Hello, world!" << std::endl; 
-	std::cout<<"i'm" << endl;
+	std::cout<<"i'm" << std::endl;
 
 	return 0;
 }   
