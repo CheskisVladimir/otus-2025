d9e402bdc5005b8cba6825b3c8be10bae2e3a95b
Vladimir Cheskis <vladimir.talmid@gmail.com>
2025-10-12 12:54:11 +0300

    Editor

---
+++ Editor/main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main(int, char **) {
+	std::cout << "Version: " << version() << std::endl;
+	std::cout << "It's Editor" << std::endl; 
+	std::cout<<"i'm" << std::endl;
+
+	return 0;
+}   
