84deafbe377604244b32caeae29c7354c8d1d8bb
Vladimir Cheskis <vladimir.talmid@gmail.com>
2025-10-19 14:09:35 +0300

    new impl1

---
+++ Editor/include/Actions.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "std_includes.h"
+
+class EditContext;
+class IDocument;
+class IMessage;
+
+class Selection;
+class Primitive;
+
+/// @brief
+class IAction
+{
+public:
+    IAction()          = default;
+    virtual ~IAction() = default;
+
+    virtual bool perform(const IMessage& message, EditContext& context, IDocument& document) = 0;
+};
+
+class AddPrimitiveAction : public IAction
+{
+public:
+    AddPrimitiveAction() = default;
+
+    bool perform(const IMessage& message, EditContext& context, IDocument& document) override;
+
+private:
+    bool get_parameters(const IMessage& message,
+                        EditContext& context,
+                        const IPrimitive& primitive,
+                        coonst Selection& selection) const;
+};
+
+class RemovePrimitiveAction : public IAction
+{
+public:
+    RemovePrimitiveAction() = default;
+
+    bool perform(const IMessage& message, EditContext& context, IDocument& document) override;
+
+private:
+    bool get_parameters(const IMessage& message,
+                        EditContext& context,
+                        coonst Selection& selection) const;
+};
