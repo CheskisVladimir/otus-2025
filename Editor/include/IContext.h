517881daf202bde88a01824f1c049e099be61535
Vladimir Cheskis <vladimir.talmid@gmail.com>
2025-10-26 15:15:17 +0300

    new diagrams

--- Editor/include/IContext.h
+++ Editor/include/IContext.h
@@ -3,10 +3,11 @@
 #include "Primitives.h"
 #include "std_includes.h"
 
-class EditContext
+class IContext
 {
 public:
-    EditContext() = default;
+    IContext()          = default;
+    virtual ~IContext() = default;
     const Selection& gt_selection() const;
     Selection& gt_selection();
 
